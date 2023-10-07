d242a5e2280388c44385bd019ba8042e9e3bb916
Akash Shaw <shawakash2003@gmail.com>
2023-10-07 21:19:57 +0530

    a simple web server looking a lot like express

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+
+use crate::models::Status;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use std::io;
+
+async fn slash() -> impl Responder  {
+    HttpResponse::Ok()
+        .json(Status {
+            status: "Ok".to_string()
+        })
+} 
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+
+    println!("Starting Server at http://localhost:3000");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(slash))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await
 }
